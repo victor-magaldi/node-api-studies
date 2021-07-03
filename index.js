75c223800e38ae0409159d06d78ad94c30b7e963
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-02 23:19:38 -0300

    usando lógica com reqparams

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ server.listen(3000)
 // queryParams = url?param1=teste&param2=teste2
 //RouteParams  = curso/2             2 seria o RouteParam
 //Request Body = {nome="victor" , teste:}
-server.get('/curso/:id', function(req,res){
+
+const cursos = ["nodejs", "javasctipt","react"]
+
+server.get('/curso/:index', function(req,res){
     // http://localhost:3000/curso/4?name=victor
     console.log(req.query.name) // victor
-    console.log(req.params.id)//4
-    return res.json({curso:"nodeJS"})
+    console.log(req.params.index)//4
+
+    const {index} = req.params
+    
+    return res.json({curso: cursos[index]})
 })
