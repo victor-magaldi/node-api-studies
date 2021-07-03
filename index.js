38c6d14f70a0bfbfaa562c078ef2204ca37a739c
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-03 13:23:24 -0300

    listagem todos cursos

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require("express")
+const express = require("express");
 
-const server = express()
-server.listen(3000)
+const server = express();
+server.listen(3000);
 
 // queryParams = url?param1=teste&param2=teste2
 //RouteParams  = curso/2             2 seria o RouteParam
 //Request Body = {nome="victor" , teste:}
 
-const cursos = ["nodejs", "javasctipt","react"]
+const cursos = ["nodejs", "javasctipt", "react"];
 
-server.get('/curso/:index', function(req,res){
-    // http://localhost:3000/curso/4?name=victor
-    console.log(req.query.name) // victor
-    console.log(req.params.index)//4
+server.get("/cursos/:id", function (req, res) {
+  // http://localhost:3000/curso/4?name=victor
+  console.log(req.query.name); // victor
+  console.log(req.params.id); //4
 
-    const {index} = req.params
-    
-    return res.json({curso: cursos[index]})
-})
+  const { id } = req.params;
+
+  return res.json({ curso: cursos[id] });
+});
+
+server.get("/cursos", function (req, res) {
+  return res.json({ cursos: cursos });
+});
