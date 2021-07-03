5620e4e18f7d247c5d72e2602fb31b020c2be57f
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-02 23:04:51 -0300

    req params e queryParams

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require("express")
 const server = express()
 server.listen(3000)
 
-server.get('/', function(req,res){
+// queryParams = url?param1=teste&param2=teste2
+//RouteParams  = curso/2             2 seria o RouteParam
+//Request Body = {nome="victor" , teste:}
+server.get('/curso/:id', function(req,res){
+    // http://localhost:3000/curso/4?name=victor
+    console.log(req.query.name) // victor
+    console.log(req.params.id)//4
     return res.json({curso:"nodeJS"})
 })
