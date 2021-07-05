d525fbe17ccbfb4dfdcc7a5fa4ef6815bcdbb1f1
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-05 19:53:21 -0300

    midlewares inicio

--- index.js
+++ index.js
@@ -23,19 +23,34 @@ server.get("/cursos/:id", function (req, res) {
   return res.json({ curso: cursos[id] });
 });
 
+// midleware Global
+// é chamado em todas rotas da sua aplicação
+server.use((req, res, next) => {
+  console.log(`REQ CHAMADA:${req.url}`);
+  return next();
+});
+
+// barra a request se o cliente não enviar o campo name
+function checkCurso(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "nome do curso é obrigatório" });
+  }
+  return next();
+}
+
 server.get("/cursos", function (req, res) {
   return res.json({ cursos: cursos });
 });
 
 // CREATE
-server.post("/cursos", function (req, res) {
+server.post("/cursos", checkCurso, function (req, res) {
   const { name } = req.body;
   cursos.push(name);
   return res.json({ cursos: cursos });
 });
 
 // UPDATE
-server.put("/cursos/:index", function (req, res) {
+server.put("/cursos/:index", checkCurso, function (req, res) {
   const { index } = req.params;
   const { name } = req.body;
   cursos[index] = name;
